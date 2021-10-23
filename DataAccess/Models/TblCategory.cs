55fd6a35183f72393c0a4e6199cb6cc9d67f5d7c
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2021-10-23 16:58:16 +0700

    Add project files.

---
+++ DataAccess/Models/TblCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataAccess.Models
+{
+    public partial class TblCategory
+    {
+        public TblCategory()
+        {
+            TblProducts = new HashSet<TblProduct>();
+        }
+
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<TblProduct> TblProducts { get; set; }
+    }
+}
---
+++ DataAccess/Models/TblOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataAccess.Models
+{
+    public partial class TblOrder
+    {
+        public TblOrder()
+        {
+            TblOrderDetails = new HashSet<TblOrderDetail>();
+        }
+
+        public Guid OrderId { get; set; }
+        public string StaffId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime? Date { get; set; }
+        public double? OrderPrice { get; set; }
+        public string StatusId { get; set; }
+        public string PaymentMethod { get; set; }
+
+        public virtual TblStaff Staff { get; set; }
+        public virtual TblStatus Status { get; set; }
+        public virtual ICollection<TblOrderDetail> TblOrderDetails { get; set; }
+    }
+}
