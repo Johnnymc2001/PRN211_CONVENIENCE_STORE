7c3b5cbe8a40a38b38b5bf7638553182c53c8115
unknown <daohuuduc.910@gmail.com>
2021-10-24 17:10:17 +0700

    Add Product DAO and Repository

---
+++ DataAccess/Repository/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IProductRepository
+    {
+        List<TblProduct> GetAllProduct();
+
+        List<TblProduct> GetAllAvailableProduct();
+
+        List<TblProduct> GetAllProductByCategory(string categoryID);
+
+        void Add(TblProduct newProduct);
+
+        void Update(TblProduct product);
+
+    }
+}
