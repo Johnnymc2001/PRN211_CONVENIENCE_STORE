6308f441d4140d20fbfefe0a084d39d8ff31dfda
Henry <henrytnh1110@gmail.com>
2021-10-24 11:35:57 +0700

    Add StaffDAO

---
+++ DataAccess/StaffDAO.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess
+{
+    class StaffDAO
+    {
+        //// THIS IS SINGLETON PATTERN ////
+
+        private static StaffDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private StaffDAO() { }
+        public static StaffDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new StaffDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        // THIS IS GET ALL STAFF
+
+        public List<TblStaff> GetAll()
+        {
+            List<TblStaff> staffs = new List<TblStaff>();
+            using (var ctx = new prn211group4Context())
+            {
+                staffs = ctx.TblStaffs.ToList();
+            }
+            return staffs;
+        }
+
+        // THIS IS GET STAFF WITH ID
+
+        public TblStaff GetUsingID(Guid staffId)
+        {
+            TblStaff staff = null;
+            using (var ctx = new prn211group4Context())
+            {
+                staff = ctx.TblStaffs.SingleOrDefault(order => order.StaffId.Equals(staffId));
+            }
+            return staff;
+        }
+
+        // THIS IS ADD NEW STAFF
+
+        public void Add(TblStaff staff)
+        {
+            using (var ctx = new prn211group4Context())
+            {
+                ctx.TblStaffs.Add(staff);
+                ctx.SaveChanges();
+            }
+        }
+
+        // THIS IS DELETE STAFF
+
+        public void Delete(TblStaff staff)
+        {
+            using (var ctx = new prn211group4Context())
+            {
+                ctx.TblStaffs.Remove(staff);
+                ctx.SaveChanges();
+            }
+        }
+
+        // THIS IS UPDATE STAFF
+        public void Update(TblStaff staff)
+        {
+            using (var ctx = new prn211group4Context())
+            {
+                ctx.Entry(staff).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
